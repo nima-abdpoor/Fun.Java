895979464b2da0f5f101f4457099d4f4d9216f3d
nimaabdpoor <nimaabdi328@yahoo.com>
2020-07-26 17:21:51 +0430

    genericMethod added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 
 
-import com.company.generics.MyMap;
+import com.company.generics.genericMethod;
 
 public class Main {
 
@@ -10,16 +10,18 @@ public class Main {
     }
 
     private static void mymap() {
-        MyMap<String,String> myMap=new MyMap<>(10);
-        myMap.put("nima","abdpoor");
-        myMap.put("mehdi","golbaz");
-        myMap.put("ali","moradi");
-        myMap.put("asad","asadi");
-        myMap.put("hasan","naghavi");
-        System.out.println(myMap.get("nima"));
-        System.out.println(myMap.get("hasan"));
-        System.out.println(myMap.get("nima"));
-        System.out.println(myMap.get("ali"));
-        System.out.println(myMap.get("mehdi"));
+        genericMethod gene =new genericMethod();
+        System.out.println(gene.add(2,3));
+//        MyMap<String,String> myMap=new MyMap<>(10);
+//        myMap.put("nima","abdpoor");
+//        myMap.put("mehdi","golbaz");
+//        myMap.put("ali","moradi");
+//        myMap.put("asad","asadi");
+//        myMap.put("hasan","naghavi");
+//        System.out.println(myMap.get("nima"));
+//        System.out.println(myMap.get("hasan"));
+//        System.out.println(myMap.get("nima"));
+//        System.out.println(myMap.get("ali"));
+//        System.out.println(myMap.get("mehdi"));
     }
 }
---
+++ src/com/company/generics/genericMethod.java
@@ -0,0 +1,7 @@
+package com.company.generics;
+
+public class genericMethod {
+    public <T extends Integer> T add(T first,T second){
+        return first;
+    }
+}
